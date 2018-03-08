37a59ac4221f12de121f59bef7eaf128d5742699
Thomas_Johnson <tjohnson1106@gmail.com>
2018-03-08 16:34:00 -0500

    added field property to foreign key to enforce snake casing in postgreSQL and leave camel casing in code-base

--- models/user.js
+++ models/user.js
@@ -1,25 +1,32 @@
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define("user", {
-    username: {
-      type: DataTypes.STRING,
-      unique: true
-    },
+  const User = sequelize.define(
+    "user",
+    {
+      username: {
+        type: DataTypes.STRING,
+        unique: true
+      },
 
-    email: {
-      type: DataTypes.STRING,
-      unique: true
-    },
+      email: {
+        type: DataTypes.STRING,
+        unique: true
+      },
 
-    password: {
-      type: DataTypes.STRING,
-      unique: true
-    }
-  });
+      password: {
+        type: DataTypes.STRING,
+        unique: true
+      }
+    },
+    { underscored: true }
+  );
 
   User.associate = models => {
     User.belongsToMany(models.Team, {
       through: "member",
-      foreignKey: "userId"
+      foreignKey: {
+        name: "userId",
+        field: "user_id"
+      }
     });
   };
 
