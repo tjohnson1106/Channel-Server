f5b2da5dd0705c570be7a603fb9681eeeac22a7a
Thomas_Johnson <tjohnson1106@gmail.com>
2018-03-08 17:14:35 -0500

    added preliminary types to gql schema

--- schema.js
+++ schema.js
@@ -1,4 +1,33 @@
 export default `
+
+type Team {
+    owner: User!
+    members: [User!]!
+    channels: [Channel!]!
+}
+
+type Channel {
+   id: Int!
+   name: String!
+   public: Boolean!
+   messages: [Message!]!  
+}
+
+type Message {
+    id: Int!
+    text: String!
+    user: User!
+    channel: Channel!
+}
+
+type User {
+    id: Int!
+    email: String!
+    password: String!
+    messages: Messages!
+    teams: [Team!]!
+}
+
 type Query {
     hi: String
 }
