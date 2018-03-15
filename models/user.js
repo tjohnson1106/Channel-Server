16ec56e547e4e706dd9989a6f0716b7c4bf8b07b
Thomas_Johnson <tjohnson1106@gmail.com>
2018-03-15 16:00:10 -0400

    created error schema

--- models/user.js
+++ models/user.js
@@ -2,17 +2,40 @@ export default (sequelize, DataTypes) => {
   const User = sequelize.define("user", {
     username: {
       type: DataTypes.STRING,
-      unique: true
+      unique: true,
+      validate: {
+        isAlphanumeric: {
+          args: true,
+          msg: "The username can only contain letters and numbers"
+        },
+        len: {
+          args: [3, 25],
+          msg: "The username needs to be between 3 and 25 characters"
+        }
+      }
     },
 
     email: {
       type: DataTypes.STRING,
-      unique: true
+      unique: true,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: "Invalid email"
+        }
+      }
     },
 
     password: {
       type: DataTypes.STRING,
-      unique: true
+      unique: true,
+      validate: {
+        len: {
+          args: [5, 100],
+          msg:
+            "The password needs to be between 5 and 100 characters long"
+        }
+      }
     }
   });
 
